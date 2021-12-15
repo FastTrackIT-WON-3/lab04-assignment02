b5d81656297786bf3884691442d86f523a127d6a
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-12-15 22:45:22 +0200

    Solve assignmnent

---
+++ Assignment02/Assignment02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assignment02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter text:");
+            string text = Console.ReadLine();
+            int words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+
+            Console.WriteLine($"Number of words: {words}");
+        }
+    }
+}
